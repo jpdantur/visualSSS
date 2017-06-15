9fc8ec8bb6963671894f9a7170a9b05ee90f49b1
Juan Pablo Dantur <wally@Juans-MacBook-Pro-2.local>
2017-06-14 21:14:12 -0300

    Argument parsing

--- src/main/java/ar/edu/itba/cripto/visualSSS/App.java
+++ src/main/java/ar/edu/itba/cripto/visualSSS/App.java
@@ -1,5 +1,15 @@
 package ar.edu.itba.cripto.visualSSS;
 
+import java.util.Arrays;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.OptionGroup;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
 /**
  * Hello world!
  *
@@ -8,6 +18,26 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	try {
+	    	Options options = new Options();
+	    	OptionGroup og = new OptionGroup();
+	    	og.setRequired(true);
+	    	og.addOption(new Option("r", "Recover image"));
+	    	og.addOption(new Option("d","Distribute image"));
+	    	options.addOptionGroup(og);
+	    	options.addRequiredOption("secret","secret",true,"Image bmp filename");
+	    	options.addRequiredOption("k", "k", true, "Minimum required shades");
+	    	options.addOption("n", true,"Shades to distribute image");
+	    	options.addOption("dir",true,"Images location");
+	    	CommandLineParser parser = new DefaultParser();
+			CommandLine line = parser.parse(options,args);
+			Option[] recognized = line.getOptions();
+			for (Option o: recognized) {
+				System.out.println(Arrays.toString(o.getValues()));
+			}
+    	} catch(ParseException e) {
+    		System.out.println("Invalid Arguments");
+    	}
+    	
     }
 }
